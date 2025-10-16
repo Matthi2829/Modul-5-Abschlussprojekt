ccdfd4824dda5788fecda4cc29725f5c60a9a8e2
Matthias <matthibauer582@gmail.com>
2025-10-16 10:06:42 +0200

    Konstruktor erstellt und getter und setter generieren lassen

--- src/Charakter.java
+++ src/Charakter.java
@@ -4,6 +4,33 @@ public class Charakter {
 
     private boolean spezialFaehigkeitAktiv;
 
+    public Charakter(String name, int leben, boolean spezialFaehigkeitAktiv) {
+        this.name = name;
+        this.leben = leben;
+        this.spezialFaehigkeitAktiv = spezialFaehigkeitAktiv;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLeben() {
+        return leben;
+    }
+
+    public void setLeben(int leben) {
+        this.leben = leben;
+    }
+
+    public boolean isSpezialFaehigkeitAktiv() {
+        return spezialFaehigkeitAktiv;
+    }
+
+    public void setSpezialFaehigkeitAktiv(boolean spezialFaehigkeitAktiv) {
+        this.spezialFaehigkeitAktiv = spezialFaehigkeitAktiv;
+    }
 }
