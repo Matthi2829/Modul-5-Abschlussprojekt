68cffe897b63dfd754ee89bee75cf505076a8c95
Erion <eprenaj@tsn.at>
2025-10-16 10:16:12 +0200

    Drachenklasse extends super gemacht und name leben und einer Spezialfähigkeit erstellt.

--- src/Drachen.java
+++ src/Drachen.java
@@ -1,3 +1,14 @@
-public class Drachen {
+import java.util.Random;
+
+public class Drachen extends Charakter {
+
+    public Drachen(String name, int leben, boolean Spezialfaehigkeit) {
+        super(name, leben, Spezialfaehigkeit);
+
+    }
+
+    public void angreifen(Charakter gegner) {
+
+    }
 
 }
