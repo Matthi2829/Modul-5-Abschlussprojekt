d5061e89fb139865070669033fae6eb0809e6f3a
Erion <eprenaj@tsn.at>
2025-10-20 15:32:14 +0200

    feinjustierung der Klasse Drache ist fertig

--- src/Drachen.java
+++ src/Drachen.java
@@ -1,24 +1,47 @@
 import java.util.Random;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Drachen extends Charakter {
 
-    public Drachen(String name, int leben, boolean Spezialfaehigkeit) {
-        super(name, leben, Spezialfaehigkeit);
-
+    public Drachen(String name, int leben, boolean spezialfaehigkeit) {
+        super(name, leben, spezialfaehigkeit);
     }
+
 
     public void angreifen(Charakter gegner) {
+        int angriff = ThreadLocalRandom.current().nextInt(20, 25 + 1); // 20-25
 
+        if (isSpezialFaehigkeitAktiv()) {
+            int malus = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+            angriff = Math.max(0, angriff - malus);
+            System.out.println(getName() + " fliegt und verliert " + malus + " Angriffspunkte!");
+        }
+
+        gegner.schadenNehmen(angriff);
+        System.out.println("Klauenangriff des " + getName() + ": " + angriff + " Schaden");
+        System.out.println(gegner.getName() + " hat noch " + gegner.getLeben() + " Leben.\n");
     }
 
-    @Override
+
     public void spezialfaehigkeitAktivieren(Charakter charakter) {
-
+        if (!isSpezialFaehigkeitAktiv()) {
+            setSpezialFaehigkeitAktiv(true);
+            setLeben(getLeben() + 10);
+            System.out.println(getName() + " aktiviert die Spezialfähigkeit 'Fliegen'! (+10 Leben)");
+        } else {
+            System.out.println("Spezialfähigkeit 'Fliegen' ist bereits aktiv!");
+        }
     }
 
-    @Override
+
     public void spezialfaehigkeitDeaktivieren() {
-
+        if (isSpezialFaehigkeitAktiv()) {
+            setSpezialFaehigkeitAktiv(false);
+            setLeben(Math.max(0, getLeben() - 10));
+            System.out.println(getName() + " deaktiviert 'Fliegen'. (-10 Leben)");
+        } else {
+            System.out.println("Spezialfähigkeit 'Fliegen' ist nicht aktiv!");
+        }
     }
-
 }
