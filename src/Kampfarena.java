b18e119ab69af324be41cd1fc32041fd086340bf
Matthias <matthibauer582@gmail.com>
2025-11-03 09:24:29 +0100

    Kampfstarten methode erstellt, push für code

--- src/Kampfarena.java
+++ src/Kampfarena.java
@@ -12,5 +12,23 @@ public class Kampfarena {
         this.rival2 = rival2;
     }
 
+    public void kampfStarten()
+    {
+        Charakter aktuellerSpieler;
+        Charakter gegner;
+
+        if (ThreadLocalRandom.current().nextBoolean()) {
+            aktuellerSpieler = rival1;
+            gegner = rival2;
+        } else {
+            aktuellerSpieler = rival2;
+            gegner = rival1;
+        }
+
+        System.out.println("Der Kampf zwischen " + rival1.getName() + " und " + rival2.getName() + "startet nun");
+        System.out.println(aktuellerSpieler.getName() + " beginnt \n");
+
+    }
+
 
 }
